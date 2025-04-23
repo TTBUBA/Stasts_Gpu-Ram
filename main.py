2cf12193548bf4f84a71a308734f07ad36e20237
BUBA <122395887+TTBUBA@users.noreply.github.com>
2025-04-23 15:06:23 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+import psutil 
+import gpustat
+import requests
+
+TOKEN = '7771087964:AAHSrOAp1Z9K7-D5MlCR3JHh-4_8ZXK9t2Y'  
+CHAT_ID = 819034844  
+cpu_usage = psutil.cpu_percent(interval=1)
+gpu_usage = gpustat.GPUStatCollection.new_query()
+ram_usage = psutil.virtual_memory().percent
+
+def send_telegram_message(cpu_usage, gpu_usage):
+        time.sleep(2)
+        for gpu in gpu_usage.gpus:
+          gpu_usage = f"GPU {gpu.name} - GPU USE {gpu.utilization}%"
+
+        Message = f"CPU Usage: {cpu_usage}%\nGPU Usage: {gpu_usage}\nRAM Usage: {ram_usage}%"
+
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+        data = {"chat_id": CHAT_ID, "text": Message}
+        response = requests.post(url, data=data)
+
+
+if __name__ == "__main__":
+    while True:
+        time.sleep(5) 
+        send_telegram_message(cpu_usage, gpu_usage)
